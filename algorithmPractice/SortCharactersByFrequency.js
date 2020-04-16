5d21cfa42c7bec1324e85d0e2e15ac5cc59a7da5
taliabahar <taliabahar@yahoo.com>
2020-04-16 02:06:10 -0700

    Create SortCharactersByFrequency.js

---
+++ algorithmPractice/SortCharactersByFrequency.js
@@ -0,0 +1,16 @@
+function frequencySort(s) {
+    if (s.length = 0) {
+        return "";
+    }
+    let letterFrequency = {};
+    let result = "";
+    for(let i=0; i < s.length; i++) {
+        let letter = s[i];
+        letterFrequency[letter] ? letterFrequency[letter]++ : letterFrequency[letter] = 1;
+    } 
+    const sortedFrequencies =  Object.keys(letterFrequency).sort((a,b)=>letterFrequency[b]-letterFrequency[a]);
+    sortedFrequencies.forEach(letter => {result += letter.repeat(letterFrequency[letter])})  
+    return result;
+};
+
+I referenced this solution to help me with the end of the algorithm: https://leetcode.com/problems/sort-characters-by-frequency/discuss/93446/JavaScript-Solution
